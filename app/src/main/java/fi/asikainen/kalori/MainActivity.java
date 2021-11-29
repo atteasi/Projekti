d091052f51b39ed33d3ea95ca00015bacb1cb9ab
atteasi <atte.asikainen@metropolia.fi>
2021-11-29 13:03:27 +0200

    Added swiping and layouts

--- app/src/main/java/fi/asikainen/kalori/MainActivity.java
+++ app/src/main/java/fi/asikainen/kalori/MainActivity.java
@@ -2,13 +2,36 @@ package fi.asikainen.kalori;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    float xStart;
+    float xEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                xStart = touchEvent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                xEnd = touchEvent.getX();
+                if(xStart < xEnd){
+                    Intent i = new Intent(MainActivity.this, SwipeLeft.class);
+                    startActivity(i);
+                }else if(xStart > xEnd){
+                    Intent i = new Intent(MainActivity.this, SwipeRight.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/fi/asikainen/kalori/SwipeRight.java
@@ -0,0 +1,13 @@
+package fi.asikainen.kalori;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SwipeRight extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.right_screen);
+    }
+}
