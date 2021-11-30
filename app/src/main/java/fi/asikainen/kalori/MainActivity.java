d2e8345b3030e8ae36c7fbfa07c6ef0d9c1c30c2
atteasi <atte.asikainen@metropolia.fi>
2021-11-30 10:05:10 +0200

    Added swipe-functionality to the SwipeRight and SwipeLeft classes

--- app/src/main/java/fi/asikainen/kalori/MainActivity.java
+++ app/src/main/java/fi/asikainen/kalori/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public boolean onTouchEvent(MotionEvent touchEvent){
         switch(touchEvent.getAction()){
             case MotionEvent.ACTION_DOWN:
