ab57dd15361b8daf4d49fb5e73b96b0919d51cff
atteasi <atte.asikainen@metropolia.fi>
2021-12-08 14:44:11 +0200

    trying to make kaloriplus work

--- app/src/main/java/fi/asikainen/kalori/KaloriPlusFragment.java
+++ app/src/main/java/fi/asikainen/kalori/KaloriPlusFragment.java
@@ -7,30 +7,33 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import java.util.ArrayList;
 
 public class KaloriPlusFragment extends Fragment {
-    final Button add = getView().findViewById(R.id.button);
-    private static ArrayList<Ruoka> ruuat = new ArrayList<>();
+//    final Button add = getView().findViewById(R.id.button);
+    private ArrayList<Ruoka> ruuat = new ArrayList<>();
+  //  EditText ruoka = getView().findViewById(R.id.ruoka);
+    //EditText kalorit = getView().findViewById(R.id.kalorit);
     private ArrayAdapter<Ruoka> listAdapter;
-    ListView ruokalista = getView().findViewById(R.id.ruokalista);
-    EditText ruoka = getView().findViewById(R.id.ruoka);
-    EditText kalorit = getView().findViewById(R.id.kalorit);
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.kalori_plus_fragment, container, false);
+        ListView ruokalista = getView().findViewById(R.id.ruokalista);
+        this.listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ruuat);
+        ruokalista.setAdapter(listAdapter);
     }
 
-
-
+/*
     public void onClick(){
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +46,7 @@ public class KaloriPlusFragment extends Fragment {
             }
         });
     }
+    */
+
 }
 
