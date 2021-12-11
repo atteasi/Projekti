d68e0b31c746a14db1f6bb17dfb6efe2a8cfb0e1
Ricardo Nunes <toni.nunes@metropolia.fi>
2021-12-11 04:54:43 +0200

    Components for Gradle and Room Database

---
+++ app/src/main/java/fi/asikainen/kalori/AppRoomDatabase.java
@@ -0,0 +1,45 @@
+package fi.asikainen.kalori;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = User.class, version = 7, exportSchema = false)
+@TypeConverters({ADBConverters.class})
+public abstract class AppRoomDatabase extends RoomDatabase {
+
+    public abstract UserDAO userDAO();
+    //public abstract CalAddDAO calAddDAO();
+    //public abstract CalSubDAO calSubDAO();
+    //public abstract WeightDAO weightDAO();
+
+    private static volatile AppRoomDatabase roomInstance;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    static final ExecutorService databaseReadExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static AppRoomDatabase getDatabase(final Context context) {
+        if (roomInstance == null) {
+            synchronized (AppRoomDatabase.class) {
+                if (roomInstance == null) {
+                    synchronized (AppRoomDatabase.class) {
+                        if (roomInstance == null) {
+                            roomInstance = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppRoomDatabase.class,
+                                    "AppDataBase").fallbackToDestructiveMigration()
+                                    .build();
+                        }
+                    }
+                }
+            }
+        }
+        return roomInstance;
+    }
+}
