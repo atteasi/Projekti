375392629e6e4a8735afca6833c01bb6bfbb5be0
atteasi <atte.asikainen@metropolia.fi>
2021-12-11 12:10:11 +0200

    Working save-feature in Kalori+ and Kalori-

--- app/src/main/java/fi/asikainen/kalori/GraphFragment.java
+++ app/src/main/java/fi/asikainen/kalori/GraphFragment.java
@@ -17,7 +17,7 @@ public class GraphFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.bmi_fragment, container, false);
+        View v = inflater.inflate(R.layout.graphs_fragment, container, false);
 
         SharedPreferences share = getActivity().getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = share.edit();
