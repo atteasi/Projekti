0fea397952d074b981f4a876f6ce125ca778d4c9
atteasi <atte.asikainen@metropolia.fi>
2021-12-11 12:02:24 +0200

    Added a Clear Data button to the App

--- app/src/main/java/fi/asikainen/kalori/GraphFragment.java
+++ app/src/main/java/fi/asikainen/kalori/GraphFragment.java
@@ -1,9 +1,12 @@
 package fi.asikainen.kalori;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,20 @@ public class GraphFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.graphs_fragment, container, false);
+        View v = inflater.inflate(R.layout.bmi_fragment, container, false);
+
+        SharedPreferences share = getActivity().getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = share.edit();
+        Button clear = (Button) v.findViewById(R.id.clearaus);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edit.remove("ruuat");
+                edit.remove("liikunnat");
+                edit.commit();
+            }
+        });
+        return v;
     }
 }
+
