8dd2e8c0bea8ddcfde6df794c09a446c2f7b4aaf
atteasi <atte.asikainen@metropolia.fi>
2021-12-12 18:16:35 +0200

    Working graphs (adjust the layout)

--- app/src/main/java/fi/asikainen/kalori/BMIFragment.java
+++ app/src/main/java/fi/asikainen/kalori/BMIFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +21,22 @@ public class BMIFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.bmi_fragment, container, false);
 
+        EditText pituus = v.findViewById(R.id.pituus);
+        EditText paino = v.findViewById(R.id.paino);
+        Button nappi = (Button) v.findViewById(R.id.nappi);
+        TextView bmiText = v.findViewById(R.id.bmi);
+
+        nappi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String height = pituus.getText().toString();
+                int pituusInt = Integer.parseInt(height);
+                String weight = paino.getText().toString();
+                int painoInt = Integer.parseInt(weight);
+                float bmi = (float) painoInt / (pituusInt * pituusInt);
+                bmiText.setText(String.valueOf(bmi));
+            }
+        });
         return v;
     }
 }
