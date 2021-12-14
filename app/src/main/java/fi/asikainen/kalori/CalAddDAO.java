1e8ca184a2f028b5d2dd33fb9203a4ec7a14b3d3
Ricardo Nunes <toni.nunes@metropolia.fi>
2021-12-14 20:26:32 +0200

    Javadocs

--- app/src/main/java/fi/asikainen/kalori/CalAddDAO.java
+++ app/src/main/java/fi/asikainen/kalori/CalAddDAO.java
@@ -9,18 +9,37 @@ import androidx.room.Query;
 
 import java.util.List;
 
+/**
+ * Data Access Object Type Room component for accessing the database. DAO
+ */
 @Dao
 public interface CalAddDAO {
-
+    /**
+     * Insert given CalAdd object to the database
+     * @param calAdd
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(CalAdd calAdd);
 
+    /**
+     * Delete given CalAdd object from the database
+     * @param calAdd
+     */
     @Delete
     void delete(CalAdd calAdd);
 
+    /**
+     * Queries the database for all CalAdd objects
+     * @return LiveData List of all CalAdd objects
+     */
     @Query("SELECT * FROM calories_added")
     LiveData<List<CalAdd>> getAllCalAdd();
 
+    /**
+     * Queries the database for given users CalAdd objects
+     * @param userID int type User identifier
+     * @return LiveData List of given
+     */
     @Query("SELECT * FROM calories_added WHERE userID = :userID ")
     LiveData<List<CalAdd>> getUsersCalAdds(int userID);
 }
--- app/src/main/java/fi/asikainen/kalori/CalSubDAO.java
+++ app/src/main/java/fi/asikainen/kalori/CalSubDAO.java
@@ -14,16 +14,32 @@ import java.util.List;
  */
 @Dao
 public interface CalSubDAO {
-
+    /**
+     * Inserts given CalSub object to the database
+     * @param calSub
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(CalSub calSub);
 
+    /**
+     * Deletes given CalSub object from the database
+     * @param calSub
+     */
     @Delete
     void delete(CalSub calSub);
 
+    /**
+     * Queries Database for all CalSub objects
+     * @return LiveData List of all CalSub objects in the database
+     */
     @Query("SELECT * FROM calories_subtracted")
     LiveData<List<CalSub>> getAllCalSub();
 
+    /**
+     * Queries Database for CalSub objects with given User identifier
+     * @param userID int type User Identifier
+     * @return LiveData List of given Users CalSubs
+     */
     @Query("SELECT * FROM calories_subtracted WHERE userID = :userID ")
     LiveData<List<CalSub>> getUsersCalSubs(int userID);
 
