32a53eff326d0d6bac057192adb85baebd3a0b60
Ricardo Nunes <toni.nunes@metropolia.fi>
2021-12-14 22:11:52 +0200

    Javadocs

--- app/src/main/java/fi/asikainen/kalori/UserDAO.java
+++ app/src/main/java/fi/asikainen/kalori/UserDAO.java
@@ -15,16 +15,33 @@ import java.util.List;
  */
 @Dao
 public interface UserDAO {
-
+    /**
+     * Insert given User type object to the database
+     * @param user
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User user);
 
+    /**
+     * Delete given user type object from the database
+     * @param user
+     */
     @Delete
     void delete(User user);
 
+    /**
+     * Query the database for all User type objects
+     * @return LiveData List of all Users
+     */
     @Query("SELECT * FROM user")
     LiveData<List<User>> getAllUsers();
 
+    /**
+     * Query the database for user of given name
+     * @param first String type first name
+     * @param last String type last name
+     * @return User object of matching name
+     */
     @Query("SELECT * FROM user WHERE user_firstname = :first AND user_lastname = :last")
     User getNamedUser(String first, String last);
 
